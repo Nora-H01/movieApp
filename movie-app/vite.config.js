2c269a9fef06692bf1b93cd5bc4b6322ed8f58f1
Nora-H01 <nora.hashani01@gmail.com>
2025-02-25 09:31:21 +0100

    fix vite.config

--- movie-app/vite.config.js
+++ movie-app/vite.config.js
@@ -4,8 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: "movie-app", // Spécifie le dossier racine
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000, // Change ici si nécessaire
+  },
+  build: {
+    outDir: "../dist", // Assure que le build est à la racine du projet
   },
 });
